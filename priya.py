de17bdc08e21105286f6919f12ad1bf8f7ea94e6
priya <114854071+prrriiya@users.noreply.github.com>
2023-03-07 17:10:05 +1000

    Add files via upload
    priya bachlor IT

---
+++ priya.py
@@ -0,0 +1,37 @@
+#TAX_RATE_LOW = 0.05  # 5%
+#TAX_RATE_HIGH = 0.1  # 10%
+#TAX_THRESHOLD_LOW = 100
+#TAX_THRESHOLD_HIGH = 1000
+
+#print("Python Party Tax Program - Where Tax is a Party")
+#income = float(input("Income: $"))
+# TODO: complete this part of the program here
+
+#if income<TAX_THRESHOLD_LOW:
+ # total_tax= 0
+#elif income>=TAX_THRESHOLD_LOW:
+#  total_tax= TAX_RATE_LOW*income
+ # take_home_pay= income-total_tax
+  #print("Toatal tax  is: $", total_tax , sep="")
+  #print("take home pay is: $", take_home_pay, sep="")
+
+
+######2CAR INSURANCE---------
+#age = input("enter age for the checking the eligiblity of the insurance - what is your age ?")
+#if int(age) ==18:
+ # print("you are refused hire altogether")
+#elif int(age) >=25:
+ # print("you are reuired to purchase special insurance")
+#elif int(age) <=25:
+ # print("not required to purchase insurance.")
+
+#4DOG
+
+
+#ageDog = int(input("What is the dog's age? "))
+#if ageDog <= 2:
+ # ageHuman = ageDog * 12
+#else:
+ # ageHuman = 24 + 6 * (ageDog - 2)
+#print("Human's age is " + str(ageHuman))
+
